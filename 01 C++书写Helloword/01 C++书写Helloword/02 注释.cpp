d99a303997b900264c1d6d8478b064b6344da827
FromStarryDawn <108778878+FromStarryDawn@users.noreply.github.com>
2022-07-08 20:13:29 +0800

    Add files via upload

---
+++ 01 C++书写Helloword/01 C++书写Helloword/02 注释.cpp
@@ -0,0 +1,10 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "HelloWord" << endl;//单行注释
+//	system("pause");/*
+//					多行注释
+//					*/
+//	return 0;
+//}
---
+++ 01 C++书写Helloword/01 C++书写Helloword/04 常量.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//常量的定义方式
+//1、#define 宏常量
+//2、const修饰的变量
+#define Day 7 
+int main()
+{
+	cout << "一周总共有：" << Day << "天" << endl;
+	const int month = 12;
+	cout << "一年总共有：" << month << "个月" << endl;
+	system("pause");
+	return 0;
+}
